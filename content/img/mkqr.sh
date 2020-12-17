1cb07e7f3b9295face1e74b8963f4a8417faee88
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2020-12-17 19:30:56 +0000

    Updated QR-code

--- content/img/mkqr.sh
+++ content/img/mkqr.sh
@@ -1,2 +1,10 @@
+# @Author: GeorgeRaven <archer>
+# @Date:   2020-12-01T12:12:37+00:00
+# @Last modified by:   archer
+# @Last modified time: 2020-12-17T19:30:10+00:00
+# @License: please see LICENSE file in project root
+
+
+
 # sudo pacman -S qrencode
-qrencode -o qrcode.png -s 4 https://github.com/DreamingRaven/2020-new-scientist-live
+qrencode -o qrcode.png -s 4 https://github.com/DreamingRaven/encrypted-deep-learning-presentation
